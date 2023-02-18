55e2aa464059cda61630d6cd089e9c4810390a09
vickyemms <64374691+vickyemms@users.noreply.github.com>
2023-02-18 19:01:26 +0100

    fifth commit

--- app/src/main/java/com/example/tipping/MainActivity.kt
+++ app/src/main/java/com/example/tipping/MainActivity.kt
@@ -8,7 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("First change")
-        this is a good day
 
 
     }
